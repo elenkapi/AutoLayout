25ee7bc559f26ae52ee051f45b5670edab2df962
ellen_kapii <elenuka.kap@gmail.com>
2022-11-03 12:34:38 +0400

    AutoLayoutDemo

---
+++ Elene_Kapanadze_13/Elene_Kapanadze_13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elene_Kapanadze_13
+//
+//  Created by Ellen_Kapii on 05.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
